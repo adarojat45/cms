3d3d2554f9df609c368432df21d0dfe6340559d1
Ajat Darojat <ajatdarojat45@gmail.com>
2020-11-01 15:25:36 +0700

    add user detail page

--- src/pages/users/Detail.js
+++ src/pages/users/Detail.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams, useHistory } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
 import {
   Card,
   CardHeader,
@@ -8,56 +7,51 @@ import {
   Container,
   Row,
   CardBody,
+  Button,
+  Col,
   TabContent,
   TabPane,
   Nav,
   NavItem,
   NavLink,
-  Button,
 } from "reactstrap";
 import classnames from "classnames";
-import { getUser, updateUser } from "../../store/actions/userAction";
-import { UserFormEdit, UserFormPassword } from "../../components/users";
+import { useDispatch, useSelector } from "react-redux";
 
-const tabTitles = ["Edit", "Password"];
+import Form from "../../components/users/Form";
+import { getUser, updateUser } from "../../store/actions/userAction";
 
 export default () => {
-  const { userId } = useParams();
-  const { user } = useSelector((state) => state.userReducer);
+  const [tabTitles] = useState(["Edit", "Password"]);
+  const [tab, setTab] = useState(1);
   const history = useHistory();
   const dispatch = useDispatch();
-  const [tab, setTab] = useState(1);
+  const { userId } = useParams();
+  const { user } = useSelector((state) => state.userReducer);
 
   useEffect(() => {
     dispatch(getUser(userId));
   }, [dispatch, userId]);
 
-  const changeTab = (tab) => {
-    setTab(tab);
-  };
-
   const renderTab = () => {
-    const tabs = [];
-    for (let i = 0; i < tabTitles.length; i++) {
-      const tabTitle = (
+    return tabTitles.map((tabTitle, i) => {
+      return (
         <NavItem key={i}>
           <NavLink
             className={classnames({ active: tab === i + 1 })}
-            onClick={() => changeTab(i + 1)}
+            onClick={() => setTab(i + 1)}
           >
             <div style={{ padding: 5 }}>
-              <b>{tabTitles[i]}</b>
+              <b>{tabTitle}</b>
             </div>
           </NavLink>
         </NavItem>
       );
-      tabs.push(tabTitle);
-    }
-    return tabs;
+    });
   };
 
-  const onSubmit = (newUser) => {
-    dispatch(updateUser(user.id, newUser));
+  const handleSubmit = (newUser) => {
+    dispatch(updateUser(userId, newUser));
   };
 
   return (
@@ -65,13 +59,13 @@ export default () => {
       <Container className="mt--7" fluid>
         <Row>
           <div className="col">
-            <Card className="shadow">
-              <CardHeader className="border-0">
-                <div className="row">
-                  <div className="col-6">
+            <Card className="bg-secondary shadow">
+              <CardHeader className="bg-white border-0">
+                <Row className="align-items-center">
+                  <Col xs="8">
                     <h3 className="mb-0">User Detail</h3>
-                  </div>
-                  <div className="text-right col-6">
+                  </Col>
+                  <Col className="text-right" xs="4">
                     <Button
                       className="btn-icon btn-2"
                       color="primary"
@@ -80,23 +74,19 @@ export default () => {
                       onClick={() => history.goBack()}
                     >
                       <span className="btn-inner--icon">
-                        <i className="fas fa-arrow-left" />
+                        <i className="ni ni-bold-left" />
                       </span>
                       Back
                     </Button>
-                  </div>
-                </div>
+                  </Col>
+                </Row>
               </CardHeader>
               <CardBody>
                 <Nav tabs>{renderTab()}</Nav>
+                <br />
                 <TabContent activeTab={tab}>
                   <TabPane tabId={1}>
-                    <br />
-                    <UserFormEdit user={user} onSubmit={onSubmit} />
-                  </TabPane>
-                  <TabPane tabId={2}>
-                    <br />
-                    <UserFormPassword />
+                    <Form user={user} onSubmit={handleSubmit} isClear={false} />
                   </TabPane>
                 </TabContent>
               </CardBody>
